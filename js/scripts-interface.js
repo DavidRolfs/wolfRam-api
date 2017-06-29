f47e08cb72da88777eb8ce7b78915151c8dcbd9a
david rolfs and joe parsley <david@>
2017-06-29 10:38:50 -0700

    add apikey to .env file

--- js/scripts-interface.js
+++ js/scripts-interface.js
@@ -1,21 +1,15 @@
-
-
 var What = require('./../js/scripts.js').whatModule;
-
+var apiKey = require('./../.env').apiKey;
 var displayResult = function(data) {
-  $('.output').append("<li>" + data + "</li>");
+  $('.output').text(data);
 };
-
-
 $(document).ready(function(){
   $("#form").submit(function(event){
     event.preventDefault();
     newWhat = new What();
     var userInput = $("#userInput").val();
-    var input = userInput.replace(/\s/g, '+')
-    console.log(input)
-
-    newWhat.GetWhat(displayResult, input);
-
+    var input = userInput.replace(/\s/g, '+');
+    console.log(input);
+    newWhat.GetWhat(displayResult, input, apiKey);
   });
 });
--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,12 @@
-
-
 What = function(){
 };
 
 var what;
 
-What.prototype.GetWhat = function(displayResult, input){
-  $.get("https://api.wolframalpha.com/v2/query?input=" + input + "%3F&format=plaintext&output=JSON&appid=EERKW7-GV9G767XUJ").then(function(response){
+What.prototype.GetWhat = function(displayResult, input, apiKey){
+  $.get("https://api.wolframalpha.com/v2/query?input=" + input + "%3F&format=plaintext&output=JSON&appid=" + apiKey ).then(function(response){
     what = JSON.parse(response);
     displayResult(what.queryresult.pods[1].subpods[0].plaintext);
-    console.log(what.queryresult.pods[1].subpods[0].plaintext);
-
   })
   .fail(function(error) {
     $('#output').text("try again");
